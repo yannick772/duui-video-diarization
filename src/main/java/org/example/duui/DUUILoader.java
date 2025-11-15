0ef38603dd1211aafd9ec13aa06a1c2bc9bdc1bf
Yannick <s2628277@uni-frankfurt.de>
2025-11-15 15:30:17 +0100

    now uses proper link for test

--- src/main/java/org/example/duui/DUUILoader.java
+++ src/main/java/org/example/duui/DUUILoader.java
@@ -1,32 +1,20 @@
 package org.example.duui;
 
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
-import org.apache.commons.compress.compressors.CompressorException;
-import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.cas.CASException;
-import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.cas.FSArrayList;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.util.InvalidXMLException;
-import org.example.duui.model.uima.VideoBase64;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.DUUIComposer;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUIDockerDriver;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUIRemoteDriver;
-import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUISwarmDriver;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUIUIMADriver;
-import org.texttechnologylab.DockerUnifiedUIMAInterface.io.writer.TTLabXmiWriter;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.lua.DUUILuaContext;
-import org.texttechnologylab.DockerUnifiedUIMAInterface.lua.LuaConsts;
-import org.texttechnologylab.DockerUnifiedUIMAInterface.pipeline_storage.sqlite.DUUISqliteStorageBackend;
 import org.texttechnologylab.annotation.type.AudioToken;
-import org.xml.sax.SAXException;
 
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Base64;
@@ -52,7 +40,8 @@ public class DUUILoader {
 
             String base64 = Base64.getEncoder().encodeToString(bytes);
             cas.setSofaDataString(base64, "video/mp4");
-
+            System.out.println("CAS created");
+            System.out.println("SofaDataString length: " + cas.getSofaDataString().length());
         } catch (ResourceInitializationException e) {
             throw new RuntimeException(e);
         } catch (CASException e) {
@@ -71,7 +60,7 @@ public class DUUILoader {
         composer.addDriver(new DUUIUIMADriver());
         composer.addDriver(new DUUIRemoteDriver());
         composer.add(
-            new DUUIRemoteDriver.Component("localhost:8000")
+            new DUUIRemoteDriver.Component("http://localhost:8000")
                     .withScale(1)
                     .withTargetView("transcript")
                     .withParameter("language", "en")
